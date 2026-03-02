94f364b22830bc4b82f9459e0a2c0d83da809a55
jenasj <jj1016@srmist.edu.in>
2026-03-02 09:59:32 +0530

    UC1: Add welcome message module

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,21 @@
+public class UseCase1PalindromeCheckerApp {
+
+    // Application Version (Constant)
+    static final String APP_VERSION = "1.0";
+
+    // Main Method - Entry Point of the Program
+    public static void main(String[] args) {
+
+        // Welcome Message
+        System.out.println("======================================");
+        System.out.println("     Welcome to Palindrome Checker Management System   ");
+        System.out.println("======================================");
+
+        // Application Details
+        System.out.println("Application Name : Palindrome Checker App");
+        System.out.println("Application Version : " + APP_VERSION);
+
+        System.out.println("\nApplication Started Successfully...");
+        System.out.println("Proceeding to next use case...");
+    }
+}
